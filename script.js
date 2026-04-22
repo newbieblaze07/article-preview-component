37e2664977fdfe862b99010c4e0d770d29e580f0
newbieblaze07 <255814481+newbieblaze07@users.noreply.github.com>
2026-04-22 09:05:40 +0545

    created desktop layout

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const smBtn=document.getElementById("sm-btn")
 const articleBox=document.querySelector(".article-box")
+const revertBtn=document.getElementById("revert-btn")
 
 smBtn.addEventListener("click",function(){
-  articleBox.classList.toggle("active")
+  articleBox.classList.add("active")
+})
+revertBtn.addEventListener("click", function(){
+  articleBox.classList.remove("active")
 })
