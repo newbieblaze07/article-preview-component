0bde2322f3ee91e42b7ef016b4fe74406b9035ea
newbieblaze07 <255814481+newbieblaze07@users.noreply.github.com>
2026-04-22 11:41:34 +0545

    headache push

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const smBtn=document.getElementById("sm-btn")
-const articleBox=document.querySelector(".article-box")
-const revertBtn=document.getElementById("revert-btn")
+const smBtn = document.getElementById("sm-btn");
+const articleBox = document.querySelector(".article-box");
+const shareBox = document.querySelector(".sm-accs");
 
-smBtn.addEventListener("click",function(){
-  articleBox.classList.add("active")
-})
-revertBtn.addEventListener("click", function(){
-  articleBox.classList.remove("active")
-})
+smBtn.addEventListener("click", function () {
+  if (window.innerWidth <= 767) {
+    articleBox.classList.toggle("active"); // mobile behavior
+  } else {
+    shareBox.classList.toggle("desktop-active"); // desktop popup
+  }
+});
